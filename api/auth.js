d4fc15913485800c86e6cfc6329b519e05d418cd
autopoet <2783924064@qq.com>
2026-03-14 21:08:28 +0800

    fix: set explicit redirect_uri for oauth

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,7 @@
 export default async function handler(req, res) {
+  const protocol = req.headers['x-forwarded-proto'] || 'http';
   const host = req.headers.host;
-  const url = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&scope=repo,user`;
+  const redirectUri = `${protocol}://${host}/api/callback`;
+  const url = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&scope=repo,user&redirect_uri=${redirectUri}`;
   res.redirect(url);
 }
