a8d5eb4e9a6ac0ddaf28f12ff9fd7083a9a23633
autopoet <2783924064@qq.com>
2026-03-22 16:02:25 +0800

    Remove admin rewrite rules from vercel.json.

--- api/auth.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const protocol = req.headers['x-forwarded-proto'] || 'http';
-  const host = req.headers.host;
-  const redirectUri = `${protocol}://${host}/api/callback`;
-  const url = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&scope=repo,user&redirect_uri=${redirectUri}`;
-  res.redirect(url);
-}
